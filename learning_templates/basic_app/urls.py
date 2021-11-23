0a2e4fa95ea486d300f898462122b86fa52c7c06
user-basic <dabhinandansharma@gmail.com>
2021-11-23 18:42:41 +0530

    'second'

--- learning_templates/basic_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
- # TEMPLATE TAGGING
-app_name = 'basic_app'  # NAMESPACING
-
-urlpatterns = [
-    path('relative/',views.relative,name='relative'),
-    path('other/',views.other,name='other'),
-]
